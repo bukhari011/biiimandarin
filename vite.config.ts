7f76809e79b75ec82e3a04a36aa06348e019376c
bukhari011 <bukharijoin@gmail.com>
2025-11-22 13:11:38 +0800

    14

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/biiimandarin/auth", 
+  base: "/biiimandarin/auth/", 
   server: {
     host: "::",
     port: 8080,
